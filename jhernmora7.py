e66bd70057c943d3c0721f09555afbbb37223d2e
jhernmora <jhernandezmora1@toromail.csudh.edu>
2025-02-26 02:23:07 +0000

    Lab 7

---
+++ jhernmora7.py
@@ -0,0 +1,23 @@
+#Jessenia Hernandez Mora
+#CSC321, Lab 7
+
+#Step 1: Create an integer, float, and string variable.
+x = 7
+y = 7.33
+z = "Stringz"
+
+#Step 2: Use selection statements to compare your variable to another integer, float, and string
+if x > 7:
+    print("x is greater than 7")
+else:
+    print("x is not greater than 7")
+
+if y > 3.77:
+    print("y is greater than 3.77")
+else:
+    print("y is not greater than 3.77")
+
+if z == "Stringz":
+    print("x is equal to Stringz")
+else:
+    print("x is not equal to Stringz")
