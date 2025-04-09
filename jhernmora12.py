bb13206e7c9bccf55f89f5b62f6054274716aa76
jhernmora <jhernandezmora1@toromail.csudh.edu>
2025-04-09 01:11:11 +0000

    Lab 12

--- jhernmora12.py
+++ jhernmora12.py
@@ -1,25 +1,25 @@
 #Jessenia Hernandez Mora
 #CSC321, Lab 12
 
-def triangle(base, height):
-    area = 0.5 * base * height
+def rectangle(base, height):
+    area = base * height
     return area
 
 def circle(radius):
     area = 3.14 * (radius * radius)
     return area
 
-#TRIANGLE
-x = int(input("Enter the base of your right triangle: "))
-y = int(input("Enter the height of your right triangle: "))
+#RECTANGLE
+x = int(input("Enter the base of your rectangle: "))
+y = int(input("Enter the height of your rectangle: "))
 
-triArea = triangle(x, y)
+rectArea = rectangle(x, y)
 
-print("The area of the triangle is: ", triArea);
+print("The area of the rectangle is: ", rectArea);
 
 #CIRCLE
 z = int(input("Enter the radius of your circle: "))
 
 circArea = circle(z)
 
-print("The area of the triangle is: ", circArea);
+print("The area of the circle is: ", circArea);
